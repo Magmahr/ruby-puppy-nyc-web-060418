e40b2af1113e9b4ae899f91aaa350f3d22ac3331
Matt Maher <mattmaher0@gmail.com>
2018-06-07 14:18:21 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class Dog
+  
+attr_accessor :name
+
+@@all = []
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+self.all
+  @@all.each do |dog|
+    puts "#{dog}"
+  end
+end
+
+self.clear_all
+  @@all.clear
+end
+
+end
+
